dc1faf1cefc07ce8e845e3766c6d00468bc00357
ayush_chandrawar <ayush_chandrawar@epam.com>
2025-08-15 12:42:36 +0530

    Added Report Generation phase

--- src/test/java/com/example/tests/tests/GoogleSearchTest.java
+++ src/test/java/com/example/tests/tests/GoogleSearchTest.java
@@ -4,24 +4,26 @@ import com.example.tests.steps.GoogleSearchSteps;
 import com.example.tests.utils.JsonDataReader;
 
 import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
 import org.assertj.core.api.Assertions;
 import java.util.Map;
 import java.util.List;
 import java.util.stream.Stream;
 
-@ExtendWith(net.serenitybdd.junit5.SerenityJUnit5Extension.class)
+@ExtendWith(SerenityJUnit5Extension.class)
 public class GoogleSearchTest {
     @Steps
     GoogleSearchSteps googleSearchSteps;
 
-    public static Stream<Map<String, Object>> searchDataProvider() {
+    public static Stream<Arguments> searchDataProvider() {
         List<Map<String, Object>> testDataList = JsonDataReader.readJsonList("searchData.json");
-        return testDataList.stream();
+        return testDataList.stream().map(Arguments::of);
     }
 
     @ParameterizedTest(name = ParameterizedTest.ARGUMENTS_WITH_NAMES_PLACEHOLDER)
